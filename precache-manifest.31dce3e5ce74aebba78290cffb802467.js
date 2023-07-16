2a82eb7b066ddf8f9e62d34a2890d16e9daa0561
zhaoxuan <zhaoxuan0914@hotmail.com>
2023-07-17 00:52:39 +0800

    added new changes

---
+++ precache-manifest.31dce3e5ce74aebba78290cffb802467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b211cba1a391ec9950cca49651997f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ffa5e0dc5aac261cadb",
+    "url": "/static/css/2.8bfa8659.chunk.css"
+  },
+  {
+    "revision": "e79db4273564e50760ba",
+    "url": "/static/css/main.4da3c915.chunk.css"
+  },
+  {
+    "revision": "3ffa5e0dc5aac261cadb",
+    "url": "/static/js/2.bd6db0c2.chunk.js"
+  },
+  {
+    "revision": "52323fc63d3a9ebc46842e47b791bdc2",
+    "url": "/static/js/2.bd6db0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79db4273564e50760ba",
+    "url": "/static/js/main.b51b1c23.chunk.js"
+  },
+  {
+    "revision": "f4800b06d7eae560929f",
+    "url": "/static/js/runtime-main.c72e126b.js"
+  }
+]);
